40a1ca953bfd2ff5babc77b0973bcf89abdd072e
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-22 22:33:37 +0300

    create news layout

--- lib/layout/news_app/cubit/states.dart
+++ lib/layout/news_app/cubit/states.dart
@@ -1,3 +1,3 @@
-// abstract class NewsStates{}
-// class NewsInitialState extends NewsStates{}
-// class NewsBottomNavState extends NewsStates{}
+abstract class NewsStates{}
+class NewsInitialState extends NewsStates{}
+class NewsBottomNavState extends NewsStates{}
