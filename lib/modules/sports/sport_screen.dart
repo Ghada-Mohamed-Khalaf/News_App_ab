97a266e3760b6d7f9413112c51b5391cd1353bd2
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-23 08:18:49 +0300

    create sport screen

--- lib/modules/sports/sport_screen.dart
+++ lib/modules/sports/sport_screen.dart
@@ -1,16 +1,16 @@
-// import 'package:flutter/material.dart';
-//
-// class SportScreen extends StatelessWidget {
-//   const SportScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Text(
-//       "Sport Screen",
-//       style: TextStyle(
-//         fontWeight: FontWeight.bold,
-//         fontSize: 20,
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class SportScreen extends StatelessWidget {
+  const SportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Sport Screen",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    );
+  }
+}
