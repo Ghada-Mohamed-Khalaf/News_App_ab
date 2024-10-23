cd98ee89dd6b31c2ca7d4458e201def1f9d8dc56
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-23 08:00:06 +0300

    create modules =>business=>businessScreen

---
+++ lib/layout/news_app/cubit/cubit.dart
@@ -0,0 +1,7 @@
+import 'package:news_app_a/layout/news_app/cubit/states.dart';
+
+class NewsCubit extends  Cubit<NewsStates>{
+  NewsCubit():super(NewsInitialState());
+  static NewsCubit  get(context)=>BlocProvider.of(context);
+
+}
