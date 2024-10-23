a4e162fae20a306c60eb397129e8e129b418bbf5
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-23 10:04:32 +0300

    add flutter bloc and bloc package

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app_a/layout/news_app/cubit/states.dart';
 
 class NewsCubit extends  Cubit<NewsStates>{
--- lib/modules/science/science_screen.dart
+++ lib/modules/science/science_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ScienceScreen extends StatelessWidget {
-  const ScienceScreen ({super.key});
+  const ScienceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Science Screen",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 20, ),);
+    return const Text(
+      "Science Screen",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    );
   }
 }
