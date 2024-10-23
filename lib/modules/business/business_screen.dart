c6e9a96fdacc129e610ed06a2228fe9774da92aa
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-10-23 08:17:32 +0300

    create business screen

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -5,6 +5,6 @@ class BusinessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text("Business Screen",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 20, ),);
   }
 }
